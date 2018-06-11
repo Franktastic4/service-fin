83d5eded54987f51181c36f5271bb57b14868d52
Frank Chung <Frank.Chung@zaplabs.com>
2018-06-10 20:34:01 -0700

    Add goals for project, Guice setup.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# service-fin
-Practice creating service.
+# ServiceFin
+
+Be a point of reference for...
+
+Libraries
+1. Hibernate, types of query and when to use. Safe use, binding.
+2. RxJava and other concurrency.
+3. Guava conventions, and tools.
+4. Lambdas (see Effective Java)
+5. Gradle
+5. Lombok
+6. Testing: Liquibase, H2, testing set-up and convention
+7. SQL
+8. Java 8/9
+
+Test with Docker and Kubernetes! 
+
+Examples...
+Guava: Make sure params are non-null unless specified.
+Effective Java examples:
+ - enums
+ - factory / constructor
+ - composition
+ - try/catch with resource
+ - mix iterative and lambdas correctly
+ - generics? 
+ - more functional programming with the interfaces
+Practice concurrency outside of Rx.
+------> Good Testing Practice
--- src/main/java/com/franktastic4/ServiceFinApplication.java
+++ src/main/java/com/franktastic4/ServiceFinApplication.java
@@ -1,29 +1,34 @@
 package com.franktastic4;
 
+import com.hubspot.dropwizard.guice.GuiceBundle;
+
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class ServiceFinApplication extends Application<service-finConfiguration> {
+public class ServiceFinApplication extends Application<ServiceFinConfiguration> {
 
-    public static void main(final String[] args) throws Exception {
-        new service-finApplication().run(args);
+    public static void main(final String[] args) throws Exception { // Entry point
+        new ServiceFinApplication().run(args);
     }
 
     @Override
     public String getName() {
-        return "service-fin";
+        return "ServiceFin";
     }
 
     @Override
-    public void initialize(final Bootstrap<service-finConfiguration> bootstrap) {
-        // TODO: application initialization
+    public void initialize(final Bootstrap<ServiceFinConfiguration> bootstrap) { // Invoked by run
+        GuiceBundle<ServiceFinConfiguration> guiceBundle = GuiceBundle.<ServiceFinConfiguration>newBuilder()
+            .enableAutoConfig(getClass().getPackage().getName()) // Part of DW, will also register your resource with Jersey.
+            .setConfigClass(ServiceFinConfiguration.class)
+            .build();
+
+        bootstrap.addBundle(guiceBundle); // TODO Tests must call JerseyGuiceUtils.reset();
     }
 
     @Override
-    public void run(final service-finConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
+    public void run(final ServiceFinConfiguration configuration, final Environment environment) {
     }
 
 }
